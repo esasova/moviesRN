ce8f7fc6e1e87ea868e77d99a52fce04e56d6fa3
esasova <sassova.elena@gmail.com>
2022-01-05 11:48:04 +0100

    add detail screen

---
+++ screens/Detail.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Text } from 'react-native'
+
+const Detail = () => {
+  return (
+    <>
+      <Text>{ 'Movie Detail' }</Text>
+    </>
+  )
+}
+
+export default Detail
+
